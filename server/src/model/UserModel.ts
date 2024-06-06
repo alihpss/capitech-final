de2a3f620064084ca37c395bdf0b929811732af3
alihpss <ali.hps@hotmail.com>
2024-06-05 21:45:24 -0300

    create user model

---
+++ server/src/model/UserModel.ts
@@ -0,0 +1,11 @@
+export default class UserModel {
+  async findByEmail(email: string) {}
+
+  async findById(id: string) {}
+
+  async createUser() {}
+
+  async updateUser() {}
+
+  async deleteUser() {}
+}
