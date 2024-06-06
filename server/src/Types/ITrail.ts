7d35da7fd5a896c913b50c2b4cd604f4a6086510
alihpss <ali.hps@hotmail.com>
2024-06-05 21:45:15 -0300

    create new types

---
+++ server/src/Types/ITrail.ts
@@ -0,0 +1,11 @@
+export interface Trail {
+  id: string;
+  caminho_img: string;
+  descricao: string;
+  descricao_video: string;
+  link: string;
+  nome: string;
+  referencias: string;
+  subtitulo: string;
+  titulo_video: string;
+}
