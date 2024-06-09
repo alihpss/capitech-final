2b7111d13c64a6a7c199f1b929b9172aeca337b1
alihpss <ali.hps@hotmail.com>
2024-06-09 12:46:00 -0300

    :sparkles: Feat: index

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { urlencoded } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import routes from './routes';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB_PATH!)
+  .then(() => {
+    const app = express();
+
+    app.use(express.json());
+    app.use(urlencoded({ extended: true }));
+    app.use(cors());
+
+    app.get('/', (_req, res) => {
+      res.status(200).json({ entry: 'API Capitech rodando!' });
+    });
+
+    app.use(routes);
+
+    app.listen(3000, () => {
+      console.log('api rodando em http://localhost:3000');
+    });
+  })
+  .catch(() => console.log('Erro ao conectar ao mongodb'));
